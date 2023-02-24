84b9139b75651991d5d6e950ea7ade183131e9aa
Aranel9966 <99angelo.dire@gmail.com>
2023-02-24 13:48:36 +0100

    scaffolding

---
+++ js/script.js
@@ -0,0 +1,18 @@
+/*
+Consegna:
+ Scrivi un programma che stampi in console i numeri da 1 a 100,
+ ma che per i multipli di 3 stampi “Fizz” al posto del numero 
+ e per i multipli di 5 stampi “Buzz”.
+  Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+*/
+
+/*
+-dichiaro la variabbile per i quadratini
+-creo in ciclo for da 1 a 100
+-ad ogni quadrato do il proprio'i'
+-?se 'i' è divisible per 3 scrivo fizz
+-?se 'i' è divisible per 5 scrivo buzz
+-?se 'i' è divisible per 5 e 3 scrivo  fizzbuzz
+
+
+*/
